094fa635d0c0dc174c99da1a5bc56c1ab8cd854d
Yu <yu@matrix.works>
2023-12-11 21:44:44 +0800

    fix: change the size of default map to 1024x1024

--- Classes/constants/MapConstants.h
+++ Classes/constants/MapConstants.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // 中间游戏区域大小，注：BLOCK_SIZE(8)的倍数
-constexpr float CENTER_WIDTH = 512;
-constexpr float CENTER_HEIGHT = 512;
+constexpr float CENTER_WIDTH = 1024;
+constexpr float CENTER_HEIGHT = 1024;
 //constexpr float CENTER_WIDTH = 256 * 4;
 //constexpr float CENTER_HEIGHT = 224 * 4;
